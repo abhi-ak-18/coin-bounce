503c144ce9d138770edf737e3086687dcb2f2289
abhi-ak-18 <58083789+abhi-ak-18@users.noreply.github.com>
2023-07-12 18:00:41 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const cors = require ('cors');
 
 //const corsOptions = {
 //  credentials: true,
-//  origin: ["http://localhost:3000","https://abhi-ak-18.github.io/coin-bounce/"],
+//  origin: ["http://localhost:3000","https://abhi-ak-18.github.io"],
 //};
 
 const app = express();
